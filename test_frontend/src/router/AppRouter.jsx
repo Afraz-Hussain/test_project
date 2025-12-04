89fc0a472dd00356f289431bada570a8e2d3f0fe
afraz <farazishah14@gmail.com>
2025-12-04 20:55:09 +0500

    added suspense and lazy loading in react app router

--- test_frontend/src/router/AppRouter.jsx
+++ test_frontend/src/router/AppRouter.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { Suspense,lazy } from 'react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ExpenseForm from '../components/expenseform/ExpenseForm';
-import ExpenseList from '../components/expenselist/ExpenseList';
-import ExpenseSummary from '../components/summary/ExpenseSummary';
+
+const ExpenseForm=lazy(()=>import ("../components/expenseform/ExpenseForm"))
+const ExpenseList=lazy(()=>import ("../components/expenselist/ExpenseList"))
+const ExpenseSummary=lazy(()=>import ("../components/summary/ExpenseSummary"))
+
 
 const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
+      <Suspense fallback={<h2>LOADING </h2>}>
         <Routes>
           <Route path="/" element={<ExpenseForm />} />
           <Route path="/list" element={<ExpenseList />} />
           <Route path="/data" element={<ExpenseSummary />} />
          
         </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
